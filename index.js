3e8ccfcb2105cfee0f52abb501fa0c1ca78aaf8c
Sarah Schaller <sarahcschaller@gmail.com>
2018-09-04 13:34:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function changeLeastFavoriteCustomer () {
 }
 
 function attemptTwoFavoriteCustomers(){
+<<<<<<< HEAD
   let favoriteCustomer = 'lucy';
   let favoriteCustomer = 'susan';
+=======
+
+>>>>>>> c7eb97d3e65795e52c4777b1c92511a7c4d20f02
 }
